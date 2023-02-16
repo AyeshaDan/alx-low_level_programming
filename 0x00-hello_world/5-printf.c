d881c547838182926799b185f815463b9574a2b8
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-02-16 09:06:39 -0800

    C program that prints with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Print "with proper grammar, but the outcome is a piece of art,"
+*
+* Return: Value 0 means successful execution of main() function.
+*/
+int main(void)
+{
+	/**
+	* printf function display the content in double quotes
+	*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
