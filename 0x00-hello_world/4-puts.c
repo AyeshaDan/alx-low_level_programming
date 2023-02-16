c66b190f88a67e2016e617eb0f297b4a19fe8009
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-02-16 08:09:41 -0800

    Fix formating issues to align with Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
- * Printxt - Prints "\"Programming is like building a multilingual puzzle"
- */
+* Printxt - Prints "\"Programming is like building a multilingual puzzle"
+*
+* Return: Value 0 means successful execution of main() function.
+*/
 int main(void)
 {
 	/**
-	 * puts function display the content in double quotes
-	 */
+	* puts function display the content in double quotes
+	*/
 	char Printxt[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Printxt); /* writing the string to stdout */
