58608874e2cdee53360bf086d968cd5f0e8d9b57
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-02-16 07:43:32 -0800

    Fix formating issues to align with Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
+
 #include <stdio.h>
+/**
+ * Printtxt - Prints "\"Programming is like building a multilingual puzzle"
+ */
 int main(void)
 {
 	/**
-	 * puts function display the content in double quotes
-	 */
+ 	* puts function display the content in double quotes
+ 	*/
 	char Printxt[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Printxt); /* writing the string to stdout */
 
-	return (0);
+	return (0);  
 }
