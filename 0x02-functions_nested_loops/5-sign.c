3a11401242c6dadb3a6f8decedf9ae957a49abe9
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-02-22 05:58:02 -0800

    Check for sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 /**
 * print_sign - the function to check for sign of a number
@@ -12,12 +11,12 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	if (n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	if (n < 0)
+	else
 	{
 		_putchar('-');
 		return (-1);
