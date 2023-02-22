788117ff7a24c60389ada507c1c22601c9bf9eef
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-02-22 04:05:10 -0800

    Print _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main - Print _putchar, followed by a new line.
+*
+* Return: Value 0 means succesful execution of main() function.
+*/
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
