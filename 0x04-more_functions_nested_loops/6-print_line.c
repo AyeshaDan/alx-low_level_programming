3a523f252b37814d4b62c73f7fd9786152550f23
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-02-24 06:41:33 -0800

    Draw a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,17 @@
 */
 void print_line(int n)
 {
+	int i;
+
 	if (n < 0)
 	{
 		_putchar('\n');
+		return;
 	}
-	else
+
+	for (i = 0; i < n; i++)
 	{
-		for (int i = 0; i < n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+		_putchar('_');
 	}
+	_putchar('\n');
 }
