d7ca879d083e76b34f40597fae2dacc856ca3c8b
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-02-24 05:59:21 -0800

    Prints 0 to 14, 10 times with new line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,12 +2,12 @@
 /**
 * more_numbers - Prints 0 to 14, 10 times with new line
 * 
-* Return: Successful execution of void more_numbers(void) function
+* Return: Successful execution of function
 */
 void more_numbers(void)
 {
 	int i = 0;
-	char c;
+	int c;
 
 	while (i < 10)
 	{
