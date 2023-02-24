71de25676856c1b75b08002ebf1a7cc8abb653c0
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-02-24 11:21:58 -0800

    Prints 0 to 14, 10 times with new line

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
 * more_numbers - Prints 0 to 14, 10 times with new line.
-* 
+*
 * Return: Successful execution of function.
 */
 void more_numbers(void)
 {
-	int i = '0';
+	int i = 0;
 	int c;
 
 	while (i < 10)
 	{
-		c = '0';
+		c = 0;
 
-		for (c = '0'; c <= '14'; c++)
+		for (c = 0; c <= 14; c++)
 		{
 			_putchar(c);
 		}
