6c75c8b762e794b9429f0452ee632388bb7563b5
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-02-24 01:31:11 -0800

    Print numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbers - Print numbers from 0 to 9
+* 
+* Return: Value 0 means succesful execution.
+*/
+void print_numbers(void)
+{
+	int print_numbers;
+
+	for (print_numbers = '0'; print_numbers <= '9'; print_numbers++)
+	{
+		_putchar(print_numbers);
+	}
+
+	_putchar('\n');
+}
