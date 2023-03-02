3476393bd051513ac572370288604eb25dce87c1
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-03-02 07:14:21 -0800

    capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+* cap_string - capitalizes all words of a string
+* Return: Capitalized string
+*/
+char *cap_string(char *ptr)
+{
+	int i;
+
+	for (i = 0; ptr[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (ptr[i] >= 'a' && ptr[i] <= 'z')
+			ptr[i] = ptr[i] - 32;
+			continue;
+		}
+		if (ptr[i] == ' ')
+		{
+			++i;
+
+			if (ptr[i] >= 'a' && ptr[i] <= 'z')
+			{
+				ptr[i] = ptr[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (ptr[i] >='A' && ptr[i] <= 'Z')
+				ptr[i] = ptr[i] + 32;
+		}
+
+	}
+
+	return (0);
+}
