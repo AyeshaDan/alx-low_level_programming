757e9e497c026f83b1ed1aac14e5297415a48b5c
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-03-02 06:53:30 -0800

    Reverse the content of an array of integers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* string_toupper - change all lowercase letters of a string to uppercase
+* string_toupper - Change all lowercase letters of a string to uppercase
 * Return: Uppercase string
 */
 char *string_toupper(char *ptr)
