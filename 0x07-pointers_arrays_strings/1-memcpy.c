6896e58f0b9d690e8586fc9e50c39ee690f1efe8
Ayesha Daniels <ayeshadaniels25@gmail.com>
2023-03-06 06:54:47 -0800

    Copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,9 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *csrc = (char *)src;
-	char *cdest = (char *)dest;
-
 	for (unsigned int i = 0; i < n; i++)
 	{
-		cdest[i] = csrc[i];
+		dest[i] = src[i];
 	}
 	return (dest);
 }
